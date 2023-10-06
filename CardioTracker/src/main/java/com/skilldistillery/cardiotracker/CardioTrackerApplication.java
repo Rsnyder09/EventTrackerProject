f7274804ba40c2f24c611939cb66167dd112ab48
Reid Snyder <reidsnyder7@gmail.com>
2023-10-06 16:56:37 -0600

    Create Spring Boot REST project, and JPA Project dependency

---
+++ CardioTracker/src/main/java/com/skilldistillery/cardiotracker/CardioTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.cardiotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardioTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardioTrackerApplication.class, args);
+	}
+
+}
---
+++ CardioTracker/src/test/java/com/skilldistillery/cardiotracker/CardioTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.cardiotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardioTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
